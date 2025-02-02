8b6ef4b7c707c6cacf53db59781508f93bfacadb
Ramika Perera <ramikaperera4@gmail.com>
2025-02-03 00:25:41 +0530

    Lowercase error

--- nexnova/nexnova-mobile.js
+++ nexnova/nexnova-mobile.js
@@ -44,7 +44,7 @@ document.querySelector('.agenda-btn').addEventListener('click', () => {
     load1.style.opacity = '1'
 
     setTimeout(() => {
-        window.location.href = '/Nexnova/agenda-mobile.html';
+        window.location.href = '/nexnova/agenda-mobile.html';
     }, 1500);
 });
 
