11696e02f25613f5c753a5fdd1b7d386cd9bfa19
Shashiya <145104853+Necros-x@users.noreply.github.com>
2025-05-17 00:46:23 +0530

    footer links

---
+++ footer.js
@@ -0,0 +1,26 @@
+function facebook() {
+  window.location.href="https://www.facebook.com/share/1GAWtrX8tL/";
+}
+
+function instagram() {
+  window.location.href="https://www.instagram.com/ssckict?igsh=MXBrejF1NDBlcDZzdA==";
+}
+
+function youtube() {
+  window.location.href="https://youtube.com/@vestersict?si=r7SCMfCdUJ26iOat";
+}
+
+function vits() {
+  window.location.href="https://vits.lk";
+}
+function about() {
+  window.location.href="https://vits.lk/about.html";
+}
+
+function contact() {
+  window.location.href="https://vits.lk/about.html#contact";
+}
+
+function schlweb() {
+  window.location.href="https://stsylvesterscollege.lk";
+}
